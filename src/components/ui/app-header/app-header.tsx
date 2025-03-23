31c63fa93e08c6f202c32fcacf25e9f76c5b4fbe
Neikos <nkalasin343@gmail.com>
2025-03-23 16:28:51 +0100

    Ver.10

--- src/components/ui/app-header/app-header.tsx
+++ src/components/ui/app-header/app-header.tsx
@@ -9,10 +9,12 @@ import {
   ProfileIcon
 } from '@zlden/react-developer-burger-ui-components';
 
+// Компонент AppHeaderUI - шапка приложения
 export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
   <header className={styles.header}>
     <nav className={`${styles.menu} p-4`}>
       <div className={styles.menu_part_left}>
+        {/* Ссылка на главную страницу */}
         <NavLink
           to='/'
           className={({ isActive }) =>
@@ -21,13 +23,15 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
         >
           {({ isActive }) => (
             <>
-              <BurgerIcon type={isActive ? 'primary' : 'secondary'} />
+              {/* Иконка "Конструктор", меняет цвет в зависимости от активности */}
+              <BurgerIcon type={isActive ? 'primary' : 'secondary'} /> 
               <p className='text text_type_main-default ml-2 mr-10'>
                 Конструктор
               </p>
             </>
           )}
         </NavLink>
+        {/* Ссылка на ленту заказов */}
         <NavLink
           to='/feed'
           className={({ isActive }) =>
@@ -36,17 +40,22 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
         >
           {({ isActive }) => (
             <>
+              {/* Иконка "Лента заказов", меняет цвет в зависимости от активности */}
               <ListIcon type={isActive ? 'primary' : 'secondary'} />
               <p className='text text_type_main-default ml-2'>Лента заказов</p>
             </>
           )}
         </NavLink>
       </div>
+      
+      {/* Логотип */}
       <div className={styles.logo}>
         <NavLink to='/'>
           <Logo className='' />
         </NavLink>
       </div>
+      
+      {/* Ссылка на профиль пользователя */}
       <NavLink
         to='/profile'
         className={({ isActive }) =>
@@ -55,6 +64,7 @@ export const AppHeaderUI: FC<TAppHeaderUIProps> = ({ userName }) => (
       >
         {({ isActive }) => (
           <div className={styles.link_position_last}>
+            {/* Иконка "Профиль", меняет цвет в зависимости от активности */}
             <ProfileIcon type={isActive ? 'primary' : 'secondary'} />
             <p className='text text_type_main-default ml-2'>
               {userName || 'Личный кабинет'}
